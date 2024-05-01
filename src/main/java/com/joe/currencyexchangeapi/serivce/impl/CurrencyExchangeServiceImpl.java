c572d383dd4519d1ec6e8a4ddcc3d01150377627
Joe Wu <99390124@ss25.mcu.edu.tw>
2024-05-01 12:57:01 +0800

    Implement convertAmountByCurrency function
    - 從ExchangeRatesConfig中獲取匯率。
    - 根據給定的匯率，計算轉換後的金額。
    - 將轉換後的金額四捨五入到小數點第二位。
    - 使用NumberFormat格式化金額，並加上千分位分隔符。

--- src/main/java/com/joe/currencyexchangeapi/serivce/impl/CurrencyExchangeServiceImpl.java
+++ src/main/java/com/joe/currencyexchangeapi/serivce/impl/CurrencyExchangeServiceImpl.java
@@ -6,7 +6,12 @@ import com.joe.currencyexchangeapi.serivce.CurrencyExchangeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Map;
 
 @Slf4j
 @Service
@@ -18,5 +23,18 @@ public class CurrencyExchangeServiceImpl implements CurrencyExchangeService {
 
     @Override
     public String convertAmountByCurrency(String sourceCurrency, String targetCurrency, BigDecimal amount) {
+
+        // 取得匯率並進行轉換
+        Map<String, Map<String, String>> currencies = exchangeRatesConfig.getCurrencies();
+
+        BigDecimal exchangeRate = BigDecimal.valueOf(Long.parseLong(currencies.get(sourceCurrency).get(targetCurrency)));
+        BigDecimal currencyExchangeAmount = amount.multiply(exchangeRate);
+
+        // 四捨五入到小數點第二位
+        currencyExchangeAmount = currencyExchangeAmount.setScale(2, RoundingMode.HALF_UP);
+
+        // 格式化金額，加上千分位分隔符
+        NumberFormat numberFormat = NumberFormat.getNumberInstance(Locale.US);
+        return numberFormat.format(currencyExchangeAmount);
     }
 }
